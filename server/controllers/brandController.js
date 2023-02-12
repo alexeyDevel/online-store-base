48c244580072d73de5c79a9fda614cd14e73d1d0
alexeyDevel <alexeydevelopermm@gmail.com>
2023-02-12 15:56:28 +0300

    Create server
    Development of the server part. Connecting to the database. Creating models. Implementation of authorization and authentication.

---
+++ server/controllers/brandController.js
@@ -0,0 +1,23 @@
+const {Brand, Type} = require("../models/models");
+
+class BrandController {
+    async create(req, res) {
+        const {name} = req.body;
+        const brand = await Brand.create({name});
+        return res.json(brand);
+    }
+    async getAll(req, res) {
+        const brands = await Brand.findAll();
+        return res.json(brands);
+    }
+    async delete(req, res){
+        const { id } = req.body;
+        const result = 
+            await Brand.destroy(
+                {where:{id}
+            });
+        return res.json(result);
+    }
+}
+
+module.exports = new BrandController();
